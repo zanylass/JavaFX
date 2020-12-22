750fb927e0d92a587a47c719c2a6fa099b77a0a5
zanylass <57338344+zanylass@users.noreply.github.com>
2020-12-23 01:51:23 +0600

    Create HeartRateCalculator.java

---
+++ HeartRateCalculator/src/HeartRate/HeartRateCalculator.java
@@ -0,0 +1,33 @@
+/*
+*********************
+* Author: Gul Meeri *
+*********************
+
+VM path
+--module-path ${PATH_TO_JAVAFX} --add-modules javafx.controls,javafx.fxml
+ */
+package HeartRate;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class HeartRateCalculator extends Application{
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("BMICalculator.fxml"));
+
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+
+        stage.setTitle("BMI Calculator"); // Displayed in window's title bar
+        stage.show();
+    }
+}
