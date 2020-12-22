09979b5517d572922ac9b190f18727cae1b746ca
zanylass <57338344+zanylass@users.noreply.github.com>
2020-12-22 23:31:08 +0600

    Update TipCalculatorMod.java

--- src/TipCalculatorMod/TipCalculatorMod.java
+++ src/TipCalculatorMod/TipCalculatorMod.java
@@ -1,4 +1,28 @@
 package TipCalculatorMod;
 
+<<<<<<< Updated upstream
 public class TipCalculatorMod {
+=======
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import static javafx.application.Application.launch;
+
+public class TipCalculatorMod {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    /*@Override
+    public void start(Stage stage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("TipCalculator.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setTitle("Tip Calculator");
+        stage.show();
+    }*/
+>>>>>>> Stashed changes
 }
