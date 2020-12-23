fde4a4d2e99748db61bd5643a0ef34dde7fdd513
zanylass <57338344+zanylass@users.noreply.github.com>
2020-12-24 02:10:25 +0600

    Update Controller.java

--- ContactsApp/src/sample/Controller.java
+++ ContactsApp/src/sample/Controller.java
@@ -1,4 +1,33 @@
+/*
+*********************
+* Author: Gul Meeri *
+*********************
+
+VM path
+--module-path ${PATH_TO_JAVAFX} --add-modules javafx.controls,javafx.fxml
+ */
+
 package sample;
 
-public class Controller {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Controller extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("ContactsApp.fxml"));
+
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+
+        stage.setTitle("Contacts App"); // Displayed in window's title bar
+        stage.show();
+    }
 }
