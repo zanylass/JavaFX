d861c1b7e2480e6a1ad6e1855e4029ea5f887be1
Gul Meeri <gulirfanmari@gmail.com>
2020-12-24 03:35:19 +0600

    updating tipcalc

--- TipCalculatorMod/src/TipCalcMod/TipCalculatorMod.java
+++ TipCalculatorMod/src/TipCalcMod/TipCalculatorMod.java
@@ -1,4 +1,33 @@
+/*
+//VM path
+--module-path ${PATH_TO_JAVAFX} --add-modules javafx.controls,javafx.fxml
+
+ */
+
 package TipCalcMod;
 
-public class TipCalculatorMod {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import static javafx.application.Application.launch;
+
+public class TipCalculatorMod extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("TipCalculatorMod.fxml"));
+
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+
+        stage.setTitle("Tip Calculator Modified"); // Displayed in window's title bar
+        stage.show();
+    }
 }
--- TipCalculatorMod/src/TipCalcMod/TipCalculatorModController.java
+++ TipCalculatorMod/src/TipCalcMod/TipCalculatorModController.java
@@ -1,4 +1,68 @@
 package TipCalcMod;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+import javafx.scene.control.TextField;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.text.NumberFormat;
+
 public class TipCalculatorModController {
+
+    private static final NumberFormat currency = NumberFormat.getCurrencyInstance();
+    private static final NumberFormat percent = NumberFormat.getPercentInstance();
+    private BigDecimal tipPercentage = new BigDecimal(0.15);
+    private BigDecimal amount = new BigDecimal(0);
+
+    @FXML private Slider tipPercentageSlider;
+    @FXML private TextField amountTextField;
+    @FXML private TextField tipTextField;
+    @FXML private TextField totalTextField;
+    @FXML private Label tipPercentageLabel;
+
+    @FXML
+    void calculate() {
+            if(amount.doubleValue() >=0) {
+                BigDecimal tip = amount.multiply(tipPercentage);
+                BigDecimal total = amount.add(tip);
+
+                tipTextField.setText(currency.format(tip));
+                totalTextField.setText(currency.format(total));
+            }else{
+                tipTextField.setText("$0");
+                totalTextField.setText("$0");
+            }
+
+
+    }
+
+    public void initialize(){
+        currency.setRoundingMode(RoundingMode.HALF_UP);
+        tipPercentageSlider.valueProperty().addListener(
+                new ChangeListener<Number>() {
+                    @Override
+                    public void changed(ObservableValue<? extends Number> observableValue, Number oldValue, Number newValue) {
+                        tipPercentage = BigDecimal.valueOf(newValue.intValue() / 100.0);
+                        tipPercentageLabel.setText(percent.format(tipPercentage));
+                        calculate();
+                    }
+                }
+        );
+
+        amountTextField.textProperty().addListener((args,oldValue,newValue) ->{
+                try {
+                    amount = new BigDecimal(newValue);
+                }catch (Exception e) {
+                    amountTextField.setText("Enter Number");
+                    amountTextField.selectAll();
+                    amountTextField.requestFocus();
+                }
+        });
+    }
+
 }
