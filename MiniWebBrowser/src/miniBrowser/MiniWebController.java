d23fbd8e135fd60cf83312f484a22fc432b5fc93
zanylass <57338344+zanylass@users.noreply.github.com>
2020-12-24 05:58:17 +0600

    Add files via upload

---
+++ MiniWebBrowser/src/miniBrowser/MiniWebController.java
@@ -0,0 +1,36 @@
+package miniBrowser;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MiniWebController implements Initializable {
+
+    @FXML
+    private WebView web;
+
+    @FXML
+    private TextField searchBar;
+
+    WebEngine webEngine;
+    String htLink = "https://";
+    String adrsLink;
+
+    @FXML
+    void goSearch(ActionEvent event) {
+        adrsLink = searchBar.getText().toString();
+        webEngine.load(htLink+ adrsLink);
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        webEngine = web.getEngine();
+        webEngine.load("https://www.google.com");
+    }
+}
