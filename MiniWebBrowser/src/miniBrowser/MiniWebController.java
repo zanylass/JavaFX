61590b06167b664e3c997b0f903af38e88b5b54c
zanylass <57338344+zanylass@users.noreply.github.com>
2020-12-24 07:30:51 +0600

    Update MiniWebController.java

--- MiniWebBrowser/src/miniBrowser/MiniWebController.java
+++ MiniWebBrowser/src/miniBrowser/MiniWebController.java
@@ -1,7 +1,4 @@
-<<<<<<< Updated upstream
-<<<<<<< HEAD
-package miniBrowser;public class MiniWebController {
-=======
+
 package miniBrowser;
 
 import javafx.event.ActionEvent;
@@ -37,43 +34,5 @@ public class MiniWebController implements Initializable {
         webEngine = web.getEngine();
         webEngine.load("https://www.google.com");
     }
->>>>>>> Stashed changes
+
 }
-=======
-package miniBrowser;
-
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.scene.control.TextField;
-import javafx.scene.web.WebEngine;
-import javafx.scene.web.WebView;
-
-import java.net.URL;
-import java.util.ResourceBundle;
-
-public class MiniWebController implements Initializable {
-
-    @FXML
-    private WebView web;
-
-    @FXML
-    private TextField searchBar;
-
-    WebEngine webEngine;
-    String htLink = "https://";
-    String adrsLink;
-
-    @FXML
-    void goSearch(ActionEvent event) {
-        adrsLink = searchBar.getText().toString();
-        webEngine.load(htLink+ adrsLink);
-    }
-
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
-        webEngine = web.getEngine();
-        webEngine.load("https://www.google.com");
-    }
-}
->>>>>>> d23fbd8e135fd60cf83312f484a22fc432b5fc93
