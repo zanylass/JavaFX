5a011ea7445527a1e9a512828cdb488274db547c
zanylass <57338344+zanylass@users.noreply.github.com>
2020-12-24 07:24:00 +0600

    Update MiniWeb.java

--- MiniWebBrowser/src/miniBrowser/MiniWeb.java
+++ MiniWebBrowser/src/miniBrowser/MiniWeb.java
@@ -1,5 +1,37 @@
+<<<<<<< Updated upstream
 <<<<<<< HEAD
 package miniBrowser;public class MiniWeb {
+=======
+package miniBrowser;
+/*
+//Author: Gul Meeri///
+//VM path
+--module-path ${PATH_TO_JAVAFX} --add-modules javafx.controls,javafx.fxml
+*/
+
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MiniWeb extends Application{
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("MiniWeb.fxml"));
+
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+
+        stage.setTitle("My mini Browser"); // Displayed in window's title bar
+        stage.show();
+    }
+>>>>>>> Stashed changes
 }
 =======
 package miniBrowser;
