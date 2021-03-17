d6c33597eec08ede05ea19756a2fc29ab8b87b64
zanylass <57338344+zanylass@users.noreply.github.com>
2021-03-17 17:45:55 +0600

    Create PersonUI.java

---
+++ PersonApp/src/modernclient/PersonUI.java
@@ -0,0 +1,36 @@
+//--module-path ${PATH_TO_JAVAFX} --add-modules javafx.controls,javafx.fxml
+
+package modernclient;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class PersonUI extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("resources/PersonUI.fxml"));
+
+        Scene scene = new Scene(root);
+
+        stage.setTitle("Person UI Example");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * The main() method is ignored in correctly deployed JavaFX application.
+     * main() serves only as fallback in case the application can not be
+     * launched through deployment artifacts, e.g., in IDEs with limited FX
+     * support. NetBeans ignores main().
+     *
+     * @param args the command line arguments
+     */
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
