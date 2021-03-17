47ba11352d45dac9ad2032b8a157215181b3ef2a
zanylass <57338344+zanylass@users.noreply.github.com>
2021-03-17 17:48:13 +0600

    Create CanvasNode.java

---
+++ PS2-CG/src/p226/CanvasNode.java
@@ -0,0 +1,24 @@
+//--module-path ${PATH_TO_JAVAFX} --add-modules javafx.controls,javafx.fxml
+package p226;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class CanvasNode extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("canvas1.fxml"));
+        primaryStage.setTitle("Canvas");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
