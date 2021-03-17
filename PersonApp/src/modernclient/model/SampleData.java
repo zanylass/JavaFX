d2616633b934438d5d927ce5d7012f9a2900dfa3
zanylass <57338344+zanylass@users.noreply.github.com>
2021-03-17 17:45:50 +0600

    Create SampleData.java

---
+++ PersonApp/src/modernclient/model/SampleData.java
@@ -0,0 +1,41 @@
+package modernclient.model;
+
+import javafx.collections.ObservableList;
+
+public class SampleData {
+    public static void fillSampleData(ObservableList<Person> backingList) {
+        backingList.add(new Person("Waldo", "Soller", "random notes 1", "Male"));
+        backingList.add(new Person("Herb", "Dinapoli", "random notes 2", "Male"));
+        backingList.add(new Person("Shawanna", "Goehring", "random notes 3", "Female"));
+        backingList.add(new Person("Flossie", "Goehring", "random notes 4", "Female"));
+        backingList.add(new Person("Magdalen", "Meadors", "random notes 5", "Female"));
+        backingList.add(new Person("Marylou", "Berube", "random notes 6", "Female"));
+        backingList.add(new Person("Ethan", "Nieto", "random notes 7", "Male"));
+        backingList.add(new Person("Elli", "Combes", "random notes 8", "Female"));
+        backingList.add(new Person("Andy", "Toupin", "random notes 9", "Male"));
+        backingList.add(new Person("Zenia", "Linwood", "random notes 10", "Female"));
+    }
+
+    /*
+    Glenn Marti
+    Waldo Soller
+    Herb Dinapoli
+    Shawanna Goehring
+    Flossie Slack
+    Magdalen Meadors
+    Marylou Berube
+    Ethan Nieto
+    Elli Combes
+    Andy Toupin
+    Zenia Linwood
+    Alan Mckeithan
+    Kattie Mellott
+    Benito Kearns
+    Lloyd Cundiff
+    Karleen Westrich
+    Jada Perrotta
+    Teofila Holbert
+    Moira Heart
+    Mitsuko Earwood
+     */
+}
