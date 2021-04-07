4884889e942b122291d57821b4bc314a1ec75c20
zanylass <57338344+zanylass@users.noreply.github.com>
2021-04-07 21:25:55 +0600

    Create Kaleidoscope.java

---
+++ PS3-CG/src/p2217/Kaleidoscope.java
@@ -0,0 +1,24 @@
+//--module-path ${PATH_TO_JAVAFX} --add-modules javafx.controls,javafx.fxml
+package p2217;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Kaleidoscope extends Application{
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Kaleidoscope");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
