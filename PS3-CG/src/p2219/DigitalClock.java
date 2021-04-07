88ab9e042a516f05cb9c51638adb746902020aeb
zanylass <57338344+zanylass@users.noreply.github.com>
2021-04-07 21:26:02 +0600

    Create DigitalClock.java

---
+++ PS3-CG/src/p2219/DigitalClock.java
@@ -0,0 +1,23 @@
+//--module-path ${PATH_TO_JAVAFX} --add-modules javafx.controls,javafx.fxml
+package p2219;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class DigitalClock extends Application{
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Digital Clock");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
