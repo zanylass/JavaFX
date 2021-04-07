54fae9d60ad1af6d9c840debf02309c28ef9bd07
zanylass <57338344+zanylass@users.noreply.github.com>
2021-04-07 21:28:29 +0600

    Update PersonUI.java

--- PersonApp/src/modernclient/PersonUI.java
+++ PersonApp/src/modernclient/PersonUI.java
@@ -16,7 +16,7 @@ public class PersonUI extends Application {
 
         Scene scene = new Scene(root);
 
-        stage.setTitle("Person UI Example");
+        stage.setTitle("Person App Mod");
         stage.setScene(scene);
         stage.show();
     }
