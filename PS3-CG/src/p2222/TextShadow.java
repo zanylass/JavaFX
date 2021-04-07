84b1c4e658c7b1639b78f525f4f3e844fcce3a87
zanylass <57338344+zanylass@users.noreply.github.com>
2021-04-07 21:26:57 +0600

    Create TextShadow.java

---
+++ PS3-CG/src/p2222/TextShadow.java
@@ -0,0 +1,23 @@
+//--module-path ${PATH_TO_JAVAFX} --add-modules javafx.controls,javafx.fxml
+package p2222;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class TextShadow extends Application{
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Text Shadow");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
