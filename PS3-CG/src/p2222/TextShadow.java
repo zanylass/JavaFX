5079f0f3dc1eceeb55bee9c605ae63d836f256e5
zanylass <57338344+zanylass@users.noreply.github.com>
2021-05-22 21:07:24 +0600

    Update TextShadow.java

--- PS3-CG/src/p2222/TextShadow.java
+++ PS3-CG/src/p2222/TextShadow.java
@@ -5,15 +5,38 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class TextShadow extends Application{
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        //Parent root = FXMLLoader.load(getClass().getResource("TextShadow.fxml"));
         primaryStage.setTitle("Text Shadow");
-        primaryStage.setScene(new Scene(root, 300, 275));
+
+        DropShadow dropShadow = new DropShadow();
+        dropShadow.setRadius(5.0);
+        dropShadow.setOffsetX(3.0);
+        dropShadow.setOffsetY(3.0);
+        dropShadow.setColor(Color.color(0.4, 0.5, 0.5));
+
+        Label textLabel = new Label("JavaFX");
+        textLabel.setFont(Font.font(null, FontWeight.BOLD, 60));
+        textLabel.setEffect(dropShadow);
+
+        StackPane root = new StackPane();
+        root.getChildren().add(textLabel);
+
+        primaryStage.setScene(new Scene(root,300,300));
+
         primaryStage.show();
+
     }
 
 
