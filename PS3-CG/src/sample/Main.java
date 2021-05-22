d7e1842a25252808e581776744831030c2e73c49
zanylass <57338344+zanylass@users.noreply.github.com>
2021-05-22 21:07:11 +0600

    Update Main.java

--- PS3-CG/src/sample/Main.java
+++ PS3-CG/src/sample/Main.java
@@ -4,6 +4,11 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -13,6 +18,24 @@ public class Main extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
+
+        DropShadow dropShadow = new DropShadow();
+        dropShadow.setRadius(5.0);
+        dropShadow.setOffsetX(3.0);
+        dropShadow.setOffsetY(3.0);
+        dropShadow.setColor(Color.color(0.4, 0.5, 0.5));
+
+        Text text = new Text();
+        text.setEffect(dropShadow);
+        text.setCache(true);
+        text.setX(10.0);
+        text.setY(70.0);
+        text.setFill(Color.web("0x3b596d"));
+        text.setText("JavaFX...");
+        text.setFont(Font.font(null, FontWeight.BOLD, 40));
+
+        
+
         primaryStage.show();
     }
 
