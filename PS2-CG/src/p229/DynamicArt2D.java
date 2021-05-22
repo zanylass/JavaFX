645542942597ca1a769ae8ad7f7e14dd2f64c711
zanylass <57338344+zanylass@users.noreply.github.com>
2021-05-22 21:02:27 +0600

    Create DynamicArt2D.java

---
+++ PS2-CG/src/p229/DynamicArt2D.java
@@ -0,0 +1,23 @@
+package p229;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class DynamicArt2D extends Application{
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("DynamicArt2D.fxml"));
+        Scene scene = new Scene(root, 600, 400);
+        primaryStage.setTitle("Dynamic Art App");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
+
